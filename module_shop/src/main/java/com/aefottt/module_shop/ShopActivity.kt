9853b0274467bd7470cd052c0404ec2d86cad8f9
aefottt <aefottt@outlook.com>
2021-08-03 13:48:08 +0800

    [test]学习git合作开发

--- module_shop/src/main/java/com/aefottt/module_shop/ShopActivity.kt
+++ module_shop/src/main/java/com/aefottt/module_shop/ShopActivity.kt
@@ -2,6 +2,7 @@ package com.aefottt.module_shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.alibaba.android.arouter.facade.annotation.Route
 import com.mredrock.cyxbs.common.config.SHOP_ENTRY
 
@@ -10,5 +11,6 @@ class ShopActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.shop_activity_main)
+        Toast.makeText(this, "这是一个Toast", Toast.LENGTH_SHORT).show()
     }
 }
