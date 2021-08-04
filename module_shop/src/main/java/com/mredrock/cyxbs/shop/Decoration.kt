ebd01dcf1b7f339e32edcadf316d74117389470c
YYQF123 <1592730587@qq.com>
2021-08-04 11:41:32 +0800

    [feat]初步完成邮票中心xml布局，规范包名

---
+++ module_shop/src/main/java/com/mredrock/cyxbs/shop/Decoration.kt
@@ -0,0 +1,29 @@
+package com.aefottt.module_shop
+
+
+import androidx.annotation.Keep
+
+@Keep
+data class Decoration(
+    val Decorations: List<Decoration>,
+    val status: Int
+) {
+    @Keep
+    data class Decoration(
+        val desc: String,
+        val id: String,
+        val imageUrl: List<ImageUrl>,
+        val imageUrls: List<ImageUrl>,
+        val leftCount: Int,
+        val periods: Int,
+        val price: Int,
+        val title: String
+    ) {
+        @Keep
+        data class ImageUrl(
+            val imageUrl1: String,
+            val imageUrl2: String
+        )
+
+    }
+}
---
+++ module_shop/src/main/java/com/mredrock/cyxbs/shop/Task.kt
@@ -0,0 +1,21 @@
+package com.aefottt.module_shop
+
+import androidx.annotation.Keep
+
+@Keep
+data class Task(
+    val count: Int,
+    val status: Int,
+    val todayTasks: List<TodayTask>
+) {
+    @Keep
+    data class TodayTask(
+            val describe: String,
+            val doneAmount: Int,
+            val isFinished: Boolean,
+            val isProgress: Boolean,
+            val rewardNumber: Int,
+            val taskName: String,
+            val totalAmount: Int
+    )
+}
